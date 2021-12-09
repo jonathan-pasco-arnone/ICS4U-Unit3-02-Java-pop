a4e19409146341dc12bbd9b96ab7d0df17c960ea
Ubuntu <ubuntu@ip-172-31-84-55.ec2.internal>
2021-12-09 17:43:41 +0000

    easy

--- MrCoxallStack.java
+++ MrCoxallStack.java
@@ -12,24 +12,31 @@ import java.util.ArrayList;
 * This is the class that contains many functions.
 */
 public final class MrCoxallStack {
-  /**
-  * Create the stack as an ArrayList.
-  */
-  private ArrayList<Integer> stackAsArray = new ArrayList<Integer>();
-
-  /**
-  * Adds a number to the ArrayList.
-  *
-  * @param pushNumber this is the inputted number to be added
-  */
-  public void push(final Integer pushNumber) {
-    stackAsArray.add(0, pushNumber);
-  }
-
-  /**
-  * Prints the ArrayList.
-  */
-  public void showStack() {
-    System.out.println(stackAsArray);
-  }
+    /**
+    * Create the stack as an ArrayList.
+    */
+    private ArrayList<Integer> stackAsArray = new ArrayList<Integer>();
+  
+    /**
+    * Adds a number to the ArrayList.
+    *
+    * @param pushNumber this is the inputted number to be added
+    */
+    public void push(final Integer pushNumber) {
+        stackAsArray.add(0, pushNumber);
+    }
+  
+    /**
+    * Pops the top number off of the ArrayList.
+    */
+    public void pop() {
+        stackAsArray.remove(0);
+    }
+  
+    /**
+    * Prints the ArrayList.
+    */
+    public void showStack() {
+        System.out.println(stackAsArray);
+    }
 }
---
+++ Pop.java
@@ -0,0 +1,62 @@
+/*
+* This program uses a custom class.
+*
+* @author  Jonathan Pasco-Arnone
+* @version 1.0
+* @since   2021-12-01
+*/
+
+/**
+* This is the main outer function.
+*/
+final class Pop {
+    /**
+    * Prevent instantiation
+    * Throw an exception IllegalStateException.
+    * if this ever is called
+    *
+    * @throws IllegalStateException
+    *
+    */
+    private Pop() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * This is the main function.
+    *
+    * @param args this will not be used
+    */
+    public static void main(final String[] args) {
+        // use MrCoxallStack class
+        final MrCoxallStack aStack = new MrCoxallStack();
+        final String addedNewNumber = "Added new number";
+        final String popNumber = "Popped the top number";
+        final String newStack = "New Stack: ";
+
+        final int fourtyTwo = 42;
+        final int ninetyOne = 91;
+        System.out.println("Initial stack: ");
+        aStack.showStack();
+
+        // Adding 42
+        System.out.println(addedNewNumber);
+        aStack.push(fourtyTwo);
+        System.out.println(newStack);
+        aStack.showStack();
+
+        // Adding 91
+        System.out.println(addedNewNumber);
+        aStack.push(ninetyOne);
+        System.out.println(newStack);
+        aStack.showStack();
+
+        // Removing the top number (currently 42)
+        System.out.println(popNumber);
+        aStack.pop();
+        aStack.showStack();
+    }
+}
